47186398557b510504468885855f6e020f7db780
KUATOproject <manuel.buczka@rub.de>
2016-09-10 00:05:47 +0200

    Layout

---
+++ index.php
@@ -0,0 +1,322 @@
+<!DOCTYPE html>
+<html lang="de">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Testumgebung">
+    <link rel="icon" href="favicon.ico">
+
+    <title>Mückenhaupt</title>
+
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/lightbox.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+    <link href="css/style.css" rel="stylesheet">
+
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <style type="text/css">.<?php echo $current; ?> >a{color:rgb(218,109,50) !important;}</style>
+  </head>
+
+  <body>
+	  <div class="container">
+		<nav class="navbar navbar-default  ">
+		  <div class="container-fluid">
+		    <!-- Brand and toggle get grouped for better mobile display -->
+		    <div class="navbar-header">
+		      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+		        <span class="sr-only">Toggle navigation</span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		      </button>
+		    </div>
+			    <div class="logo"></div>
+		    <!-- Collect the nav links, forms, and other content for toggling -->
+		    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+		      <ul class="nav navbar-nav">
+		        <li class="about"><a href="?about">Über uns</a></li>
+		        <li class="leistungen"><a href="?about">Leistungen</a></li>
+		        <li class="quali"><a href="?about">Qualifikation</a></li>
+		        <li class="kontakt"><a href="?about">Kontakt</a></li>
+		      </ul>
+		        </li>
+		      </ul>
+		    </div><!-- /.navbar-collapse -->
+		  </div><!-- /.container-fluid -->
+		</nav>
+	  </div>
+
+	  <!-- Jumbotron -->
+    <div class="jumbotron" ></div>
+
+	<!-- CTA -->
+    <div class="container">
+	    <div class="cta_bar">
+		    <div class="teaser">
+			    Ihr zertifizierter Entsorgungsfachbetrieb im Kreis Paderborn
+		    </div>
+		    <button class="cta_btn">
+			    test
+		    </button>
+	    </div>
+    </div>
+
+	<!-- Unsere Leistungen -->
+    <div class="container">
+	    <div class="zentriert">
+		    <h1>
+			   Unsere Leistungen
+		    </h1>
+		    <h2>
+			    Für Gewerbetreibende und Privatpersonen
+		    </h2>
+		    <hr />
+		    <p class="center-block">
+			    Text Tetggg gdg dg ergt ererter erter
+		    </p>
+	    </div>
+	   <div class="row zentriert">
+		   <div class="col-md-3">
+			   Kasten
+		   </div>
+		   <div class="col-md-3">
+			   Kasten
+		   </div>
+		   <div class="col-md-3">
+			   Kasten
+		   </div>
+		   <div class="col-md-3">
+			   Kasten
+		   </div>
+	   </div>
+    </div>
+
+	<!-- Über uns -->
+    <div class="container">
+	    <div class="col-md-6">
+		    <h1>
+			   Über uns
+		    </h1>
+		    <h2>
+			    Familienunternehmen in der 3. Generation
+		    </h2>
+		    <hr />
+		    <p>
+			    Text Tetggg gdg dg ergt ererter erter Text Tetggg gdg dg ergt ererter erter Text Tetggg gdg dg ergt ererter erter Text Tetggg gdg dg ergt ererter erter Text Tetggg gdg dg ergt ererter erter Text Tetggg gdg dg ergt ererter erter
+		    </p>
+		    <p>
+			    Text Tetggg gdg dg ergt ererter erter Text Tetggg gdg dg ergt ererter erter Text Tetggg gdg dg ergt ererter erter Text Tetggg gdg dg ergt ererter erter Text Tetggg gdg dg ergt ererter erter Text Tetggg gdg dg ergt ererter erter Text Tetggg gdg dg ergt ererter erter Text Tetggg gdg dg ergt ererter erter Text Tetggg gdg dg ergt ererter erter Text Tetggg gdg dg ergt ererter erter Text Tetggg gdg dg ergt ererter erter Text Tetggg gdg dg ergt ererter erter Text Tetggg gdg dg ergt ererter erter Text Tetggg gdg dg ergt ererter erter Text Tetggg gdg dg ergt ererter erter Text Tetggg gdg dg ergt ererter erter Text Tetggg gdg dg ergt ererter erter Text Tetggg gdg dg ergt ererter erter
+		    </p>
+	    </div>
+	    <div class="col-md-6">
+	     	Img Slider
+	    </div>
+    </div>
+
+    <!-- Zitate -->
+    <div class="container">
+	    <div class="col-md-12">
+		    <div class="zitate zentriert">
+			    Zitate
+		    </div>
+	    </div>
+    </div>
+
+    <!-- Galerie -->
+    <div class="container">
+	    <div class="col-md-12">
+		    <div class="zentriert">
+			    <h1>Bildergalerie</h1>
+			    <hr/>
+			    <button class="gallery_btn">Fuhrpark</button>
+			    <button class="gallery_btn">Fuhrpark</button>
+			    <button class="gallery_btn">Fuhrpark</button>
+			    <button class="gallery_btn">Fuhrpark</button>
+		    </div>
+	    </div>
+	    <div class="col-md-2 thumbnail">
+		    Thumbnail
+		</div>
+		<div class="col-md-2 thumbnail">
+		    Thumbnail
+		</div>
+		<div class="col-md-2 thumbnail">
+		    Thumbnail
+		</div>
+		<div class="col-md-2 thumbnail">
+		    Thumbnail
+		</div>
+		<div class="col-md-2 thumbnail">
+		    Thumbnail
+		</div>
+		<div class="col-md-2 thumbnail">
+		    Thumbnail
+		</div>
+    </div>
+
+    <!-- Qualifizierung -->
+    <div class="container">
+	    <div class="col-md-12">
+		    <div class=" zentriert">
+			    <h1>Qualifizierung</h1>
+			    <h2>Zertifizierter Entsorgungsfachbetrieb, der Umwelt zuliebe!</h2>
+				<hr>
+				<div class="col-md-6">
+					TExt links
+				</div>
+				<div class="col-md-6">
+					TExt rechts
+				</div>
+		    </div>
+	    </div>
+    </div>
+
+     <!-- Downloads -->
+    <div class="container">
+	    <div class="col-md-12">
+		    <div class="zentriert">
+			    <h1>Downloads</h1>
+			    <h2>Zertifikate und Siegel</h2>
+				<hr>
+				<div class="col-md-2 thumbnail">
+					Thumbnail
+				</div>
+				<div class="col-md-2 thumbnail">
+					Thumbnail
+				</div>
+				<div class="col-md-2 thumbnail">
+					Thumbnail
+				</div>
+				<div class="col-md-2 thumbnail">
+					Thumbnail
+				</div>
+				<div class="col-md-2 thumbnail">
+					Thumbnail
+				</div>
+				<div class="col-md-2 thumbnail">
+					Thumbnail
+				</div>
+	    </div>
+    </div>
+
+	<!-- Kontakt -->
+    <div class="container">
+	    <div class="col-md-12">
+		    <div class="zentriert">
+			    <h1>Kontaktieren Sie uns</h1>
+			    <h2>Wir freuen uns auf Sie!</h2>
+				<hr>
+				<p>
+					testsetrt rt erte rtert eter ter
+				</p>
+		    </div>
+	    </div>
+	    <div class="zentriert">
+		    <div class="col-md-4">
+			    Mail
+			</div>
+			<div class="col-md-4">
+			    Adresse
+			</div>
+			<div class="col-md-4">
+			    Telefon
+		    </div>
+	    </div>
+	    <div class="col-md-6">
+		    Form links
+	    </div>
+	    <div class="col-md-6">
+		    Form rechts
+		    <button>Senden</button>
+	    </div>
+    </div>
+
+    <!-- Anfahrt -->
+    <div class="container">
+	    <div class="col-md-12">
+		    <div class="zentriert">
+			    <h1>Anfahrt</h1>
+			    <h2>So finden Sie uns!</h2>
+				<hr>
+				<div class="map">
+					Google Map
+				</div>
+		   </div>
+	    </div>
+    </div>
+
+    <!-- Unsere Partner -->
+    <div class="container">
+	    <div class="col-md-12">
+		    <div class="zentriert">
+			    <h1>Unsere Partner</h1>
+			    <h2>Gemeinsam für Ihren Erfolg</h2>
+				<hr>
+				<div class="col-md-3 thumbnail">
+					Thumbnail
+				</div>
+				<div class="col-md-3 thumbnail">
+					Thumbnail
+				</div>
+				<div class="col-md-3 thumbnail">
+					Thumbnail
+				</div>
+				<div class="col-md-3 thumbnail">
+					Thumbnail
+				</div>
+	    </div>
+    </div>
+
+    <!-- Footer  -->
+    <div class="container">
+	    <div class="col-md-12">
+				<div class="col-md-2 ">
+					<h1>Allgemein</h1>
+					<ul>
+						<li>eintrag</li>
+						<li>eintrag</li>
+						<li>eintrag</li>
+						<li>eintrag</li>
+						<li>eintrag</li>
+						<li>eintrag</li>
+						<li>eintrag</li>
+					</ul>
+				</div>
+				<div class="col-md-2 ">
+					<h1>Leistungen</h1>
+					<ul>
+						<li>eintrag</li>
+						<li>eintrag</li>
+						<li>eintrag</li>
+						<li>eintrag</li>
+						<li>eintrag</li>
+					</ul>
+				</div>
+				<div class="col-md-2 ">
+					<h1>Kontakt</h1>
+					Adolf Mückenhaupt<br>
+					GmbH & Co. KG<br>
+					Brubebachstr. 43<br>
+					Gewerbegebiet Westenholz<br>
+					33129 Delbrück<br>
+					Tel.: 02944/97 10-0<br>
+					Fax: 02944/97 10-14
+				</div>
+				<div class="col-md-6">
+					Logo
+				</div>
+	    </div>
+    </div>
+
+
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/lightbox.js"></script>
+  </body>
+</html>
